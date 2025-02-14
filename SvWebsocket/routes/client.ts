32e818c96c3433ffb12160c8cdfe68363c8d95cf
kaijun123 <kaijun123@gmail.com>
2025-02-14 18:01:52 +0800

    update client

--- SvWebsocket/routes/client.ts
+++ SvWebsocket/routes/client.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import path from "path"
 
 const controller = (req: Request, res: Response, next: NextFunction) => {
-  res.sendFile(path.join(__dirname, '../../static/index.html'));
+  res.sendFile(path.join(__dirname, '../static/index.html'));
 }
 
 export default controller
